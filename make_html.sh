2ab9b21bc816e118a6fd7041d85b24bc554bc712
Labasse <labasse@gmail.com>
2022-12-08 19:03:48 +0100

    Sources, data and assets

---
+++ make_html.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cat data/_liste.txt data/fg67hIOrf.txt | java -cp biblio.jar epsi.biblio.App > wwwroot/fg67hIOrf.html
+cat data/_liste.txt data/rt25TYuy0.txt | java -cp biblio.jar epsi.biblio.App > wwwroot/rt25TYuy0.html
