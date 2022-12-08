4f76b1a2d837c9ece2146b9a3810b85c87487065
Labasse <labasse@gmail.com>
2022-12-08 22:50:17 +0100

    Customizable jar file name in make_html

--- make_html.sh
+++ make_html.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-cat data/_liste.txt data/fg67hIOrf.txt | java -cp biblio.jar epsi.biblio.App > wwwroot/fg67hIOrf.html
-cat data/_liste.txt data/rt25TYuy0.txt | java -cp biblio.jar epsi.biblio.App > wwwroot/rt25TYuy0.html
+JARFILE="${1:-biblio.jar}"
+
+cat data/_liste.txt data/fg67hIOrf.txt | java -cp $JARFILE epsi.biblio.App > wwwroot/fg67hIOrf.html
+cat data/_liste.txt data/rt25TYuy0.txt | java -cp $JARFILE epsi.biblio.App > wwwroot/rt25TYuy0.html
